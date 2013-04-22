848c78baae3e22e8b6629068902f548be6745bc7
Alex B <alex.cheesecakey@googlemail.com>
2013-04-22 12:53:36 +0100

    Forgot to add the comments to the test suite

--- personalOrganiser/PersonalOrganiserTestSuite.java
+++ personalOrganiser/PersonalOrganiserTestSuite.java
@@ -4,6 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * Collects together and runs all test cases for the personal organiser
+ * @author Alexander
+ * @version 1.0
+ */
 @RunWith(Suite.class)
 @SuiteClasses({AppointmentTest.class, ContactTest.class, TodoTest.class})
 public class PersonalOrganiserTestSuite {
